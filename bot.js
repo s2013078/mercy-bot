351e8ccda5d86fc4d84421fd08122cfd21adeaf4
s2013078 <32028516+s2013078@users.noreply.github.com>
2018-01-23 16:52:40 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,22 +1,7 @@
-var Discord = require('discord.io');
-var logger = require('winston');
-var auth = require('./auth.json');
-// Configure logger settings
-logger.remove(logger.transports.Console);
-logger.add(logger.transports.Console, {
-    colorize: true
-});
-logger.level = 'debug';
-// Initialize Discord Bot
-var bot = new Discord.Client({
-   token: auth.token,
-   autorun: true
-});
-bot.on('ready', function (evt) {
-    logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
-});
+const Discord = require('discord.io');
+const logger = require('winston');
+const auth = require('./auth.json');
+
 bot.on('message', function (user, userID, channelID, message, evt) {
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
@@ -159,7 +144,7 @@ case 'tonyking':
                     message: '神一般的存在!'
 					});
             break;
-        
+
          }
      }
 });
